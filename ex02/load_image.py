11e1a09033d07448dd4079ae6ca95a8f3d1dde92
Kaouthar Kouaz <kkouaz@e2r1p12.1337.ma>
2025-11-17 04:48:50 +0100

    enjoying riles music

---
+++ ex02/load_image.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.image as mpimg
+
+
+def ft_load(path: str) -> np.ndarray:
+    """
+    Loads an image from the given path, prints its shape and pixel content,
+    and returns the pixel array. Handles JPG and JPEG formats and errors.
+    """
+    if not path.lower().endswith(('.jpg', '.jpeg')):
+        print("Error: File format must be JPG or JPEG.")
+        return None
+    try:
+        img_arr = mpimg.imread(path)
+        print(f"The shape of image is: {img_arr.shape}")
+        print(img_arr)
+        return img_arr
+    except Exception as e:
+        print(f"error : {e} ")
+
+
+def main():
+
+    print(ft_load("landscape.jpg"))
+    print(ft_load("test.png"))
+
+
+if __name__ == "__main__":
+    main()
